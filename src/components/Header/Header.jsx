0a265c94f16b804730f113247c8100e32d16c6e7
Amit biswas <amitbiswas9282@gmail.com>
2025-01-08 00:53:04 +0600

    NavLink Modify

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div className="flex gap-8 justify-center">
-        <NavLink className="btn bg-accent" onClick={`/login`}>Login</NavLink>
-        <NavLink className="btn bg-accent" onClick={`/sign-up`}>Sign Up</NavLink>
+    <div className="flex gap-8 justify-center py-4">
+        <NavLink className="btn bg-accent" to={`/login`}>Login</NavLink>
+        <NavLink className="btn bg-accent" to={`/sign-up`}>Sign Up</NavLink>
     </div>
   )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router-dom"
-import Header from "../Header/Header"
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
 
 const Main = () => {
   return (
-    <div>
-        <Header/>
-        <Outlet/>
+    <div className="bg-gray-500">
+      <Header />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
